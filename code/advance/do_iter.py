5ec568a086e6fcc0c0fa6875aff17958f92ac0a1
lyh165 <454361733+lyh165@users.noreply.github.com>
2018-02-08 21:23:30 +0800

    2018-02-08-6.2、迭代Code
    2018-02-08-6.2、迭代Code.

---
+++ code/advance/do_iter.py
@@ -0,0 +1,102 @@
+# 由于Python源代码也是一个文本文件，所以，当你的源代码中包含中文的时候，在保存源代码时，就需要务必指定保存为UTF-8编码。
+# 当Python解释器读取源代码时，为了让它按UTF-8编码读取，我们通常在文件开头写上这两行：
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 迭代 <iteration>
+
+"""
+如果给定一个list或tuple，我们可以通过for循环来遍历这个list或tuple，
+这种遍历我们称为迭代（Iteration）。
+在Python中，迭代是通过for ... in来完成的，
+而很多语言比如C语言，迭代list是通过下标完成的，比如Java代码：
+
+for (i=0; i<list.length; i++) {
+    n = list[i];
+}
+"""
+
+
+
+
+"""
+一、
+可以看出，Python的for循环抽象程度要高于C的for循环，
+因为Python的for循环不仅可以用在list或tuple上，还可以作用在其他可迭代对象上。
+
+list这种数据类型虽然有下标，但很多其他数据类型是没有下标的，
+但是，只要是可迭代对象，无论有无下标，都可以迭代，比如dict就可以迭代：
+"""
+
+d = {'a':1,'b':2,'c':3}
+for key in d:
+	print(key)
+
+print('---')
+"""
+因为dict的存储不是按照list的方式顺序排列，所以，迭代出的结果顺序很可能不一样。
+默认情况下，dict迭代的是key。
+如果要迭代value，可以用for value in d.values()，
+如果要同时迭代key和value，可以用for k, v in d.items()。
+"""
+# 二、由于字符串也是可迭代对象，因此，也可以作用于for循环：
+for ch in 'ABC':
+	print(ch)
+
+
+"""
+三、判断是不是迭代对象
+所以，当我们使用for循环时，只要作用于一个可迭代对象，
+for循环就可以正常运行，而我们不太关心该对象究竟是list还是其他数据类型。
+那么，如何判断一个对象是可迭代对象呢？
+方法是通过collections模块的Iterable类型判断：
+"""
+
+from collections import Iterable
+
+print(isinstance('abc', Iterable)) # str是否可迭代
+print(isinstance([1,2,3], Iterable)) # list是否可迭代
+print(isinstance(123, Iterable)) # 整数是否可迭代
+
+"""
+最后一个小问题，如果要对list实现类似Java那样的下标循环怎么办？
+Python内置的enumerate函数可以把一个list变成索引-元素对，这样就可以在for循环中同时迭代索引和元素本身：
+"""
+for i,vlaue in enumerate(['A','B','C']):
+	print(i,vlaue)
+print('---')
+# 上面的for循环里，同时引用了两个变量，在Python里是很常见的，比如下面的代码：
+for x,y in [(1,1),(2,4),(3,9)]:
+	print(x,y)
+
+
+"""
+练习
+请使用迭代查找一个list中最小和最大值，并返回一个tuple：
+"""
+def findMinAndMax(L):
+	if len(L) == 0:
+		return (None,None)
+	else:
+		min = L[0]
+		max = L[0]
+		for x in L:
+			if min > x:
+				min = x
+			if x > max:
+				max = x
+		return min,max
+
+L = [5,3,2,1,12]
+
+print(findMinAndMax(L))
+if findMinAndMax([]) != (None, None):
+    print('测试失败!')
+elif findMinAndMax([7]) != (7, 7):
+    print('测试失败!')
+elif findMinAndMax([7, 1]) != (1, 7):
+    print('测试失败!')
+elif findMinAndMax([7, 1, 3, 9, 5]) != (1, 9):
+    print('测试失败!')
+else:
+    print('测试成功!')
